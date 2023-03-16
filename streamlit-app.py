a4a1ca97c571e9940f44b4e28f0f6fa656d03277
TMatteus95 <61378788+TMatteus95@users.noreply.github.com>
2023-03-16 14:58:13 +1100

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -12,7 +12,7 @@ restaurants = pd.DataFrame(columns = ['id', 'restaurant', 'url', 'newspaper', 'g
                            data = [[1,'HolySmoke BBQ', 'dn.se','Dagens Nyheter', 56.260860, 12.550790, 'red'],
                                    [2,'Vedens lustgård', 'gp.se','Göteborgs Posten',58.426000,13.464320, 'blue']])
 
-restaurants_selected = st.multiselect("Filter which restaurants you want to see the reviews of:", list(restaurants.iloc[:,1]), list(restaurants.iloc[:,1]))
+restaurants_selected = st.sidebar.multiselect("Filter which restaurants you want to see the reviews of:", list(restaurants.iloc[:,1]), list(restaurants.iloc[:,1]))
 
 restaurants_to_show = restaurants.loc[restaurants.loc[:,'restaurant'].isin(restaurants_selected)]
 st.dataframe(restaurants_to_show)
@@ -75,15 +75,4 @@ folium.TileLayer('cartodbdark_matter').add_to(m)
 st_data = st_folium(m, width=700)
 
 
-# Using object notation
-add_selectbox = st.sidebar.selectbox(
-    "How would you like to be contacted?",
-    ("Email", "Home phone", "Mobile phone")
-)
 
-# Using "with" notation
-with st.sidebar:
-    add_radio = st.radio(
-        "Choose a shipping method",
-        ("Standard (5-15 days)", "Express (2-5 days)")
-    )
