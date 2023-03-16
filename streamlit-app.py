afdafc1ae1e1f60fb4690f9a1c52c37853ad3456
TMatteus95 <61378788+TMatteus95@users.noreply.github.com>
2023-03-16 13:50:21 +1100

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -14,7 +14,7 @@ restaurants = pd.DataFrame(columns = ['id', 'restaurant', 'url', 'newspaper', 'g
 restaurants_selected = st.multiselect("Filter which restaurants you want to see the reviews of:", list(restaurants.iloc[:,1]), list(restaurants.iloc[:,1]))
 
 restaurants_to_show = restaurants.loc[restaurants.loc[:,'restaurant'].isin(restaurants_selected)]
-st.dataframe(restaurants)
+st.dataframe(restaurants_to_show)
 
 # Iitiate the map with a start location of gothenburg
 m = folium.Map(location=[57.708870, 11.974560], zoom_start=6)
