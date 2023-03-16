767489bc50094aded722fde7f7ca5aa0c85e9e79
TMatteus95 <61378788+TMatteus95@users.noreply.github.com>
2023-03-16 12:20:32 +1100

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -1,4 +1,4 @@
-import streamlit
+import streamlit as st
 import pandas as pd 
 import requests
 import snowflake.connector
@@ -6,16 +6,17 @@ from urllib.error import URLError
 import folium
 from streamlit_folium import st_folium, folium_static
 
-restaurants = pd.DataFrame(columns = ['id', 'restaurant', 'url', 'newspaper', 'gps_lat', 'gps_long'],
-                           data = [[1,'HolySmoke BBQ', 'dn.se','Dagens Nyheter', 56.260860, 12.550790],
-                                   [2,'Vedens lustgård', 'gp.se','Göteborgs Posten',58.426000,13.464320]])
+# Get data
+restaurants = pd.DataFrame(columns = ['id', 'restaurant', 'url', 'newspaper', 'gps_lat', 'gps_long', 'color_marker'],
+                           data = [[1,'HolySmoke BBQ', 'dn.se','Dagens Nyheter', 56.260860, 12.550790, 'red'],
+                                   [2,'Vedens lustgård', 'gp.se','Göteborgs Posten',58.426000,13.464320, 'blue']])
 
 
-
+# Iitiate the map with a start location of gothenburg
 m = folium.Map(location=[57.708870, 11.974560], zoom_start=6)
 
+# Adding a marker
 tooltip = "Click me!"
-
 folium.Marker(
     [56.260860, 12.550790], popup="<p>Holy Smoke BBQ</p> <p>Recesent: Dagen nyheter  Betyg: 5/5 L&auml;nk: https://www.dn.se/kultur/holy-smoke-skansk-barbecue-pa-riktigt/</p>", tooltip=tooltip
 ).add_to(m)
@@ -23,8 +24,6 @@ folium.TileLayer('cartodbdark_matter').add_to(m)
 
 st_data = st_folium(m, width=700)
 
-
-import streamlit as st
 
 # Using object notation
 add_selectbox = st.sidebar.selectbox(
