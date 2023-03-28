6cdcffab33fd0c7d96f1a8e5a88ccaa79f1c0b88
TMatteus95 <61378788+TMatteus95@users.noreply.github.com>
2023-03-29 10:53:04 +1100

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -6,6 +6,9 @@ import snowflake.connector
 from urllib.error import URLError
 import folium
 from streamlit_folium import st_folium, folium_static
+import datetime as dt
+from dateutil.relativedelta import relativedelta # to add days or years
+
 
 # Get data
 restaurants = pd.DataFrame(columns = ['id', 'restaurant', 'url', 'newspaper', 'gps_lat', 'gps_long', 'color_marker'],
@@ -15,6 +18,21 @@ restaurants = pd.DataFrame(columns = ['id', 'restaurant', 'url', 'newspaper', 'g
 restaurants_selected = st.sidebar.multiselect("Jag vill se recensioner från följande tidningar:", list(restaurants.iloc[:,3]), list(restaurants.iloc[:,3]))
 
 restaurants_to_show = restaurants.loc[restaurants.loc[:,'newspaper'].isin(restaurants_selected)]
+
+## Range selector
+cols1,_ = st.beta_columns((1,2)) # To make it narrower
+format = 'MMM DD, YYYY'  # format output
+start_date = dt.date(year=2021,month=1,day=1)-relativedelta(years=2)  #  I need some range in the past
+end_date = dt.datetime.now().date()-relativedelta(years=2)
+max_days = end_date-start_date
+
+slider = cols1.slider('Select date', min_value=start_date, value=end_date ,max_value=end_date, format=format)
+## Sanity check
+st.table(pd.DataFrame([[start_date, slider, end_date]],
+              columns=['start',
+                       'selected',
+                       'end'],
+              index=['date']))
 
 
 # Initiate the map with a start location of gothenburg
